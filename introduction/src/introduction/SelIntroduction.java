e403472f73eaad22f873db105ee8379f24666c8c
albiton <52779534+albiton@users.noreply.github.com>
2025-07-22 16:42:50 +0900

    Add files via upload

---
+++ introduction/src/introduction/SelIntroduction.java
@@ -0,0 +1,22 @@
+package introduction;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelIntroduction {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+
+		WebDriver driver1 = new FirefoxDriver();
+
+		WebDriver driver2 = new EdgeDriver();
+		
+		driver.get("https://rahulshettyacademy.com/");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		driver.quit();
+	}
+
+}
---
+++ introduction/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module intorduction {
+	requires org.seleniumhq.selenium.chrome_driver;
+	requires org.seleniumhq.selenium.api;
+}
